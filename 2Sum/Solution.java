89b559370b18f98ce7b96715911a3d33b3c4abd3
piyush041093 <52488036+piyush041093@users.noreply.github.com>
2022-03-29 11:43:36 -0400

    Update Solution.java

--- 2Sum/Solution.java
+++ 2Sum/Solution.java
@@ -1,17 +1,19 @@
-class Solution:
-    # This Method will return 2 sum index of target list.
-    # @twoSum - list of integers
-    # @target - Integer representing target.
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return []
-        numDic = {}
-        # Iterate over list.
-        for i in range (len(nums)):
-            curr =  nums[i]
-            var = target - curr
-            # If difference between target and int at i th position is in the vals then return their intexes. else add the index and value to the vals.
-            if var in numDic:
-                return [numDic[var],i]
-            numDic[curr] = i
-        return []
+class Solution {
+    // This Method will return 2 sum index of target list.
+    // @twoSum - list of integers
+    // @target - Integer representing target.
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> vals = new HashMap<Integer, Integer>();
+        int diff = 0;
+        // Iterate over list.
+        for (int i = 0; i<nums.length;i++){
+            diff = target - nums[i];
+            // If difference between target and int at i th position is in the vals then return their intexes. else add the index and value to the vals.
+            if (vals.containsKey(diff)) {
+                return new int[] {vals.get(diff), i};
+            }
+            vals.put(nums[i],i);
+        }
+        return new int[] {};
+    }
+}
